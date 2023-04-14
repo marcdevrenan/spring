0b5384de41afca4c1afeae38c7bed8db16d14c1e
Renan Marcilio <Renan.Marcilio@leste.com>
2023-04-14 10:33:02 -0300

    perf: transactional annotation

--- backend/src/main/java/com/example/spring/services/EnterpriseService.java
+++ backend/src/main/java/com/example/spring/services/EnterpriseService.java
@@ -4,6 +4,7 @@ import com.example.spring.models.Enterprise;
 import com.example.spring.repositories.EnterpriseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class EnterpriseService {
     @Autowired
     private EnterpriseRepository enterpriseRepository;
 
+    @Transactional(readOnly = true)
     public List<Enterprise> findAll() {
         return enterpriseRepository.findAll();
     }
