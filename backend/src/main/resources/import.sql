c1ddc9b209081de3352bc8a17268fd2158569c88
Renan Marcilio <Renan.Marcilio@leste.com>
2023-04-14 10:43:47 -0300

    env: database seeding test

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO enterprises(name) VALUES ('Kojima Productions Co.');
+INSERT INTO enterprises(name) VALUES ('Valve Corporation');
+INSERT INTO enterprises(name) VALUES ('FromSoftware, Inc.');
