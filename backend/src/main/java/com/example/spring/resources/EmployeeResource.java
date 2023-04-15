5bf3a918d8a6de3f979341076911606e964cacf1
Renan Marcilio <Renan.Marcilio@leste.com>
2023-04-15 11:10:46 -0300

    feat: employee resource

---
+++ backend/src/main/java/com/example/spring/resources/EmployeeResource.java
@@ -0,0 +1,71 @@
+package com.example.spring.resources;
+
+import com.example.spring.dto.EmployeeDTO;
+import com.example.spring.dto.EnterpriseDTO;
+import com.example.spring.services.EmployeeService;
+import com.example.spring.services.EnterpriseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/enterprises")
+public class EmployeeResource {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+//    @GetMapping
+//    public ResponseEntity<Page<EnterpriseDTO>> findAll(
+//            @RequestParam(value = "page", defaultValue = "0") Integer page,
+//            @RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+//            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+//            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+//    ) {
+//        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+//
+//        Page<EnterpriseDTO> list = enterpriseService.findAllPaged(pageRequest);
+//
+//        return ResponseEntity.ok().body(list);
+//    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> findAll() {
+        List<EmployeeDTO> list = employeeService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id) {
+        EmployeeDTO dto = employeeService.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+        dto = employeeService.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EmployeeDTO> findById(@PathVariable Long id, @RequestBody EmployeeDTO dto) {
+        dto = employeeService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        employeeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
