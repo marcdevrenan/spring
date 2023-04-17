0e85015c330fecb8eef7d220c724b31389934208
Renan Marcilio <Renan.Marcilio@leste.com>
2023-04-17 17:46:57 -0300

    feat: role models

---
+++ backend/src/main/java/com/example/spring/models/Role.java
@@ -0,0 +1,39 @@
+package com.example.spring.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
